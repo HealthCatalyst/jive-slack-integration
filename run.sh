88e7992169ae1d851669014d9d6ac5b9e6dbc951
cedar-ave <marla.sowards@healthcatalyst.com>
2018-10-10 13:11:41 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Add placeID: https://<your jive instance>/api/core/v3/places?filter=type(space,group)&fields=-resources,placeID,displayName,-id,-typeCode&count=100&startIndex=0
 
+chmod +x *.sh
+
 # placeID
 ./1234.sh
 
